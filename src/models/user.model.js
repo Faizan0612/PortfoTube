f39d828a438a04639ecd419cd0792cc0dc1f8df3
Faizan0612 <asrarifaizan97@gmail.com>
2024-01-30 02:07:39 +0530

    Added user and video models

---
+++ src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String,  // cloudinary URL
+            required: true
+        },
+        coverImage: {
+            type: String,  // cloudinary URL
+        },
+        watchHistory: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+const user = mongoose.model("User", userSchema);
+
+export default user;
